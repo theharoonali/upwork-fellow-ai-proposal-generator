91121791bc476a1a7845bbcb9b58656ef510102c
Muhammad Haroon <muhammadharoon1592@gmail.com>
2024-06-08 15:16:15 +0500

    upadated

---
+++ src/components/form.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+
+const Form = () => {
+  const [response, setResponse] = useState();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleFetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://express-app-typescript.vercel.app/test"
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const data = await response.json();
+      setResponse(data.message);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission
+    console.log(formData);
+  };
+
+  return (
+    <div style={{ minWidth: "500px", padding: "14px", minHeight: "500px" }}>
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "20px",
+        }}
+      >
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <textarea
+          name="message"
+          placeholder="Message"
+          value={formData.message}
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+
+      <h3>Backend Check:</h3>
+      <button onClick={handleFetchData}>Click to check</button>
+      <p>{response ? response : "Backend API not working"}</p>
+    </div>
+  );
+};
+
+export default Form;
