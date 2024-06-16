d582e5ba031b6036e398c2de4ee39e578cfffda5
Muhammad Haroon <muhammadharoon1592@gmail.com>
2024-06-16 16:48:52 +0500

    updated

--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -11,16 +11,6 @@ function Main() {
     setStep(1);
   };
 
-  const onNextStep = () => {
-    setStep((prevStep) => prevStep + 1);
-    console.log("Next Step Clicked");
-  };
-
-  const onPreviousStep = () => {
-    setStep((prevStep) => prevStep - 1);
-    console.log("Previous Step Clicked");
-  };
-
   const onClickBack = () => {
     setStep(0);
   };
@@ -30,7 +20,6 @@ function Main() {
   };
 
   const onSaveTemplate = () => {
-    console.log("tempalate Saved");
     setStep(0);
   };
 
@@ -40,32 +29,21 @@ function Main() {
 
   return (
     <>
-      <div style={{ height: "100%", width: "100%" }}>
+      <div>
         <Header
           onClick={onClickBack}
           backDisable={step === 0}
           onClickSettings={onClickSettings}
         />
-        {step === 0 && (
-          <div
-            style={{
-              height: "100%",
-              padding: "20px",
-              backgroundColor: "#f7f7f7",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <div
-              style={{
-                width: "100%",
-                height: "100%",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "space-between",
-              }}
-            >
+        <div
+          style={{
+            padding: "20px",
+            backgroundColor: "#f7f7f7",
+            height: "450px",
+          }}
+        >
+          {step === 0 && (
+            <div className="main-section">
               <div
                 style={{
                   display: "flex",
@@ -81,113 +59,32 @@ function Main() {
                 <Button text="Create Template" onClick={onCreateTemplate} />
               </div>
             </div>
-          </div>
-        )}
-        {step === 1 && (
-          <div
-            style={{
-              height: "100%",
-              padding: "20px",
-              backgroundColor: "#f7f7f7",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <div
-              style={{
-                width: "100%",
-                height: "100%",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "space-between",
-              }}
-            >
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "12px",
-                }}
-              >
-                Create template fields
-              </div>
+          )}
+          {step === 1 && (
+            <div className="main-section">
+              <div className="sub-main-section">Create template fields</div>
               <div>
                 <Button text="Save Template" onClick={onSaveTemplate} />
               </div>
             </div>
-          </div>
-        )}
-        {step === 2 && (
-          <div
-            style={{
-              height: "100%",
-              padding: "20px",
-              backgroundColor: "#f7f7f7",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <div
-              style={{
-                width: "100%",
-                height: "100%",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "space-between",
-              }}
-            >
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "12px",
-                }}
-              >
-                Generate Proposal
-              </div>
+          )}
+          {step === 2 && (
+            <div className="main-section">
+              <div className="sub-main-section">Generate Proposal</div>
               <div>
                 <Button text="Regenrate" />
               </div>
             </div>
-          </div>
-        )}
-        {step === 3 && (
-          <div
-            style={{
-              height: "100%",
-              padding: "20px",
-              backgroundColor: "#f7f7f7",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <div
-              style={{
-                width: "100%",
-                height: "100%",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "space-between",
-              }}
-            >
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "12px",
-                }}
-              >
-                Settings
-              </div>
+          )}
+          {step === 3 && (
+            <div className="main-section">
+              <div className="sub-main-section">Settings</div>
               <div>
                 <Button text="Save" />
               </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </>
   );
