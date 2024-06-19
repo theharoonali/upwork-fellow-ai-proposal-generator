518e6b67b136088dff544a8324068b61e14da200
Muhammad Haroon <muhammadharoon1592@gmail.com>
2024-06-19 15:08:49 +0500

    updated

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 dotenv.config();
 
 app.get("/", async (req: Request, res: Response) => {
-  res.send("Server is running!")
+  res.send("Server is running!");
 });
 
 app.post("/generate-proposal", async (req: Request, res: Response) => {
@@ -23,23 +23,25 @@ app.post("/generate-proposal", async (req: Request, res: Response) => {
       email ? ` and my email is ${email}` : ""
     }. I am a ${template}. ${
       instructions ? "AI Instructions: " + instructions + ". " : ""
-    }\n\nMy Job Details:\n`;
+    } ${jobDetails ? "\n\nMy Job Details:\n" : ""}`;
 
-    jobDetails.forEach((job, index) => {
-      proposalText += `${index + 1}. Title: ${job.title}, Experience: ${
-        job.experience
-      } years, Job Description / Stack: ${job.description}\n`;
-    });
+    if (jobDetails) {
+      jobDetails.forEach((job, index) => {
+        proposalText += `${index + 1}. Title: ${job.title}, Experience: ${
+          job.experience
+        } years, Job Description / Stack: ${job.description}\n`;
+      });
+    }
 
     const openai = new OpenAI({
       apiKey: apiKey || process.env.OPENAI_API_KEY,
     });
 
-    const systemMessage = `${proposalText}\nThis is the job description provided by some client: ${proposal}\n\nStart with Dear Client, Create a compelling proposal that speaks directly to your needs, adding a personal touch throughout. Ensure each paragraph ends naturally. Please ensure the text does not appear artificially generated. Do not add any fillers like for example [name], just provide me the proposal. Add double </br> tag after each paragraph.`;
+    const systemMessage = `${proposalText}\nThis is the job description provided by some client: ${proposal}\n\nStart with Dear Client, Create a compelling proposal that speaks directly to your needs, adding a personal touch throughout. Ensure each paragraph ends naturally. Please ensure the text does not appear artificially generated. Do not add any fillers like for example [name], just provide me the proposal. Important to add </br> tag after each paragraph. (Important note: only talk about my relivant job experience to the client's description)`;
 
     const completion = await openai.chat.completions.create({
       messages: [{ role: "system", content: systemMessage }],
-      model: 'gpt-4o',
+      model: "gpt-4o",
     });
 
     const responseContent = completion.choices[0].message.content;
